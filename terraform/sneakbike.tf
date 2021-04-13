ce5b1e7e4cb6a3b8827de0671d9394c94cac0a15
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-04-13 02:37:23 -0500

    chore: slimmed the remote exec call;

--- terraform/sneakbike.tf
+++ terraform/sneakbike.tf
@@ -66,28 +66,19 @@ resource "aws_instance" "sneakbike_instance" {
     host        = self.public_ip
   }
 
-  # TODO: This is so gross, make it all one thing or something.
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get update",
-      "sudo apt-get install -y wget unzip software-properties-common dpkg-dev git make gcc automake build-essential zlib1g-dev libpcre3 libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd-dev libgeoip-dev libgoogle-perftools-dev libperl-dev pkg-config autotools-dev gpac ffmpeg mediainfo mencoder lame libvorbisenc2 libvorbisfile3 libx264-dev libvo-aacenc-dev libmp3lame-dev libopus-dev unzip",
-      "sudo add-apt-repository -y ppa:nginx/stable && sudo apt-get update",
-      "sudo apt-get install -y nginx",
-      "sudo apt-get install -y libnginx-mod-rtmp",
-      "sudo echo \"rtmp {\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"   server {\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"        listen 1935;\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"        chunk_size 4096;\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"        max_streams 512;\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"        application live {\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"                live on; \" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"                record off;\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"        }\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"    }\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo echo \"}\" | sudo tee -a /etc/nginx/nginx.conf",
-      "sudo systemctl restart nginx",
-      "sudo systemctl restart nginx"
+    inline = [<<EOF
+      sleep 1 && \
+      sudo add-apt-repository -y ppa:nginx/stable && \
+      sleep 1 && \
+      sudo apt-get update && \
+      sleep 1 && \
+      sudo apt-get install -y libnginx-mod-rtmp && \
+      sleep 1 && \
+      sudo echo "rtmp { server { listen 1935; chunk_size 4096; max_streams 512; application live { live on; record off; } } }" | sudo tee -a /etc/nginx/nginx.conf
+
+      sudo systemctl restart nginx
+    EOF
     ]
 
   }
